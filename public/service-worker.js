9cbdef8b7791f06b1ae8a4ea58fd2f0d9ba207b5
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-01-31 14:53:26 -0500

    add gitignore for node_modules and updates to index.html and service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,8 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/css/styles.css",
     "/js/idb.js",
+    "/js/index.js",
+    "/manifest.json",
     "/icons/icon-72x72.png",
     "/icons/icon-96x96.png",
     "/icons/icon-128x128.png",
